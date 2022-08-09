9fb5eaea1918dea4281227de8aced2c2cefe7868
NER <NER.Alliance@gmail.com>
2022-08-09 14:12:10 +0200

    Update gradle to 7.5 Fix JUnit 5 in buildscript Fix naming in gitignore

--- JVM/build.gradle.kts
+++ JVM/build.gradle.kts
@@ -1,9 +1,40 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+repositories {
+    gradlePluginPortal()
+    mavenCentral()
+    mavenLocal()
+    maven {
+        url = uri("https://maven.pkg.jetbrains.space/alliance-software/p/dngames/kyukez")
+        credentials {
+            username = "4f275f73-3678-49d0-b749-16a16347d97f"
+            password = "eyJhbGciOiJSUzUxMiJ9.eyJzdWIiOiI0ZjI3NWY3My0zNjc4LTQ5ZDAtYjc0OS0xNmExNjM0N2Q5N2YiLCJhdWQiOiI0ZjI3NWY3My0zNjc4LTQ5ZDAtYjc0OS0xNmExNjM0N2Q5N2YiLCJvcmdEb21haW4iOiJhbGxpYW5jZS1zb2Z0d2FyZSIsIm5hbWUiOiJLeXVrZXoiLCJpc3MiOiJodHRwczpcL1wvYWxsaWFuY2Utc29mdHdhcmUuamV0YnJhaW5zLnNwYWNlIiwicGVybV90b2tlbiI6IjVXRzRqME9OSkc0IiwicHJpbmNpcGFsX3R5cGUiOiJTRVJWSUNFIiwiaWF0IjoxNjQzNjI5MzAzfQ.kFXjgVOK7uf71hZZMsPISzhV6OnUkXw3SFsHPj9HjlE1o2C9vqyzkIyG7tCHyzIl8e-qNsXTJk4qOOmpGyxeDlKu5Qenhjdt9usUwLSCPzRMfhxliUyYC7U58I0cmLz5qazR69dKKwL9g9oqk_b_qNCv1XMj4uFPLEVBHv_uI2A"
+        }
+    }
+}
+
+plugins {
+    java
+    `java-library`
+    kotlin("jvm") version "1.7.20-Beta"
+
+    // Plugin which checks for dependency updates with help/dependencyUpdates task.
+    id("com.github.ben-manes.versions") version "0.42.0"
+
+    // Plugin which can update Gradle dependencies, use help/useLatestVersions
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+
+    `maven-publish`
+
+    // Test coverage
+    jacoco
+}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
         mavenCentral()
+        mavenLocal()
         maven {
             url = uri("https://maven.pkg.jetbrains.space/alliance-software/p/dngames/kyukez")
             credentials {
@@ -14,15 +45,37 @@ buildscript {
     }
 }
 
-plugins {
-    kotlin("jvm") version "1.7.10"
-    `java-library`
-    `maven-publish`
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
-tasks.create<Delete>("clean-output") {
-    group = "clean"
-    delete(rootProject.buildDir)
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+
+tasks {
+    create<Delete>("clean-output") {
+        group = "clean"
+        delete(rootProject.buildDir)
+    }
+
+    test {
+        useJUnitPlatform {
+            includeEngines("junit-jupiter")
+            // excludeEngines("junit-vintage")
+        }
+
+        systemProperty("junit.jupiter.conditions.deactivate", "*")
+        systemProperty("junit.jupiter.extensions.autodetection.enabled", true)
+        systemProperty("junit.jupiter.testinstance.lifecycle.default", "per_class")
+    }
 }
 
 afterEvaluate {
@@ -48,40 +101,34 @@ afterEvaluate {
     }
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.24")
     annotationProcessor("org.projectlombok:lombok:1.18.24")
 
-    implementation("org.jetbrains:annotations:23.0.0")
-
     testCompileOnly("org.projectlombok:lombok:1.18.24")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+
+    implementation("org.jetbrains:annotations:23.0.0")
+
     implementation(kotlin("stdlib-jdk8"))
-}
+    implementation(kotlin("reflect"))
+    implementation(kotlin("test"))
+    implementation(kotlin("test-junit"))
 
-repositories {
-    mavenCentral()
-    maven {
-        url = uri("https://maven.pkg.jetbrains.space/alliance-software/p/dngames/kyukez")
-        credentials {
-            username = "4f275f73-3678-49d0-b749-16a16347d97f"
-            password = "eyJhbGciOiJSUzUxMiJ9.eyJzdWIiOiI0ZjI3NWY3My0zNjc4LTQ5ZDAtYjc0OS0xNmExNjM0N2Q5N2YiLCJhdWQiOiI0ZjI3NWY3My0zNjc4LTQ5ZDAtYjc0OS0xNmExNjM0N2Q5N2YiLCJvcmdEb21haW4iOiJhbGxpYW5jZS1zb2Z0d2FyZSIsIm5hbWUiOiJLeXVrZXoiLCJpc3MiOiJodHRwczpcL1wvYWxsaWFuY2Utc29mdHdhcmUuamV0YnJhaW5zLnNwYWNlIiwicGVybV90b2tlbiI6IjVXRzRqME9OSkc0IiwicHJpbmNpcGFsX3R5cGUiOiJTRVJWSUNFIiwiaWF0IjoxNjQzNjI5MzAzfQ.kFXjgVOK7uf71hZZMsPISzhV6OnUkXw3SFsHPj9HjlE1o2C9vqyzkIyG7tCHyzIl8e-qNsXTJk4qOOmpGyxeDlKu5Qenhjdt9usUwLSCPzRMfhxliUyYC7U58I0cmLz5qazR69dKKwL9g9oqk_b_qNCv1XMj4uFPLEVBHv_uI2A"
-        }
-    }
-}
+    // JUnit 5
+    testImplementation(platform("org.junit:junit-bom:5.9.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.9.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-console:1.9.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-engine:1.9.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-reporting:1.9.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-commons:1.9.0")
 
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    // Kotlintest
+    testImplementation("io.kotlintest:kotlintest-core:3.4.2")
+    testImplementation("io.kotlintest:kotlintest-assertions:3.4.2")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 }
